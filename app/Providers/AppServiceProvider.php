030baae76e017a97a21221601c49467fac3f56e1
dee <dagoon07@gmail.com>
2025-09-07 10:53:48 +0200

    dockerfile removed cached

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,10 @@ class AppServiceProvider extends ServiceProvider
         });
     }
 
-    public function boot(): void
+   public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
