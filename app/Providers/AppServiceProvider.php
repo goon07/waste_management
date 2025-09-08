f3630b1bcc9ee677b655bda7caae714a17507860
dee <dagoon07@gmail.com>
2025-09-08 10:23:06 +0200

    json and everything removed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Services\SupabaseAuth; // <-- ADD THIS
 
 class AppServiceProvider extends ServiceProvider
@@ -17,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
    public function boot(): void
     {
         if ($this->app->environment('production')) {
-            URL::forceScheme('https');
+           URL::forceScheme('https');
         }
     }
 }
