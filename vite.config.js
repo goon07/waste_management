bf8fc2e68ddc26916071da01266c74f311dfd7f1
dee <dagoon07@gmail.com>
2025-09-08 11:02:42 +0200

    json and everything removed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css', // Add this if it exists
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
